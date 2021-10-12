c97396aa483ec97757299ff81306907706f6d22a
Alejandro Villaescusa <57651541+alexvillhen@users.noreply.github.com>
2021-10-12 07:47:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,28 @@ let rowTop = document.querySelector("#rowTop");
 let rowMid = document.querySelector("#rowMid");
 let rowBot = document.querySelector("#rowBot");
 
+
 function toggleMenu() {
     if(menuOn == false){
         menu.style.top = "45px";
+        rowMid.style.right = "-150px";
+        rowTop.style.transform = "rotate(45deg)";
+        rowTop.style.top = "11.5px";
+        rowBot.style.transform = "rotate(-45deg)";
+        rowBot.style.bottom = "11.5px";
         menuOn = true;
+
+        
     } 
     else{
         menu.style.top = "-500px";
+        rowMid.style.right = "0px";
+        rowTop.style.transform = "rotate(0deg)";
+        rowTop.style.top = "0px";
+        rowBot.style.transform = "rotate(0deg)";
+        rowBot.style.bottom = "0px";
         menuOn = false;
     }
-}
-
-function buttonAnimation(){
-
 }
 
 menuButton.addEventListener("click", toggleMenu);
